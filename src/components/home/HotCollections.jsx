68076b94b6d6786088f6d4e771f64684509f0093
NunezAN <alex101nunez@gmail.com>
2022-09-26 15:52:27 -0500

    added axios dependency and fetching and displaying data

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const HotCollections = () => {
+  const [hotCollectionsData, setHotCollectionsData] = useState([]);
+
+  useEffect(()=> {
+    async function getHotCollections(){
+      const fetchedData = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
+      setHotCollectionsData(fetchedData.data);
+    }
+    getHotCollections();
+
+  },[])
+
+  console.log(hotCollectionsData);
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,7 +27,7 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {/* {new Array(4).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
@@ -36,6 +49,29 @@ const HotCollections = () => {
                 </div>
               </div>
             </div>
+          ))} */}
+          {hotCollectionsData.map(data=>(
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={data.id}>
+            <div className="nft_coll">
+              <div className="nft_wrap">
+                <Link to={`/item-details/${data.nftId}`}>
+                  <img src={data.nftImage} className="lazy img-fluid" alt="" />
+                </Link>
+              </div>
+              <div className="nft_coll_pp">
+                <Link to= {`/author/${data.authorId}`}>
+                  <img className="lazy pp-coll" src={data.authorImage} alt="" />
+                </Link>
+                <i className="fa fa-check"></i>
+              </div>
+              <div className="nft_coll_info">
+                <Link to="/explore">
+                  <h4>{data.title}</h4>
+                </Link>
+                <span>ERC-{data.code}</span>
+              </div>
+            </div>
+          </div>
           ))}
         </div>
       </div>
