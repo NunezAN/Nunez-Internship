986fd4d378ecbe7035ee6fc6ac76092e68d6ddac
NunezAN <alex101nunez@gmail.com>
2022-09-27 09:14:46 -0500

    carousel is responsive

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -8,43 +8,47 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
-  const [hotCollectionsData, setHotCollectionsData] = useState([]);
+  const [hotCollectionsData, setHotCollectionsData] = useState([]);   //state to store fetched nft data
 
-  var settings = {
+  var settings = {    //variables for the carousel animations
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
     initialSlide: 0,
-    responsive: [
+    responsive: [           //responsive breakpoints for carousel
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          initialSlide: 0,
+          infinite: true,
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 0
+          slidesToScroll: 1,
+          initialSlide: 1,
+          infinite: true,
         }
       },
       {
-        breakpoint: 1024,
+        breakpoint: 1070,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
+          initialSlide: 2,
           infinite: true,
         }
       }
     ]
   };
 
-  useEffect(() => {
+  useEffect(() => {                             //fetchdata once on load
     async function getHotCollections() {
       const fetchedData = await axios.get(
         "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
@@ -54,7 +58,7 @@ const HotCollections = () => {
     getHotCollections();
   }, []);
 
-  console.log(hotCollectionsData);
+  
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -65,33 +69,10 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {/* {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))} */}
-          <Slider {...settings}>
+          {/* map thorugh array of fetched data into html */}
+          <Slider {...settings}>                    
             {hotCollectionsData.map((data) => (
               <div
-                // className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
                 key={data.id}
               >
                 <div className="nft_coll">
