c45234736bea9693f30a187f4e38adc20f732e4c
NunezAN <alex101nunez@gmail.com>
2022-09-26 21:52:01 -0500

    carousel is added, need to complete responsiveness

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -3,18 +3,56 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
   const [hotCollectionsData, setHotCollectionsData] = useState([]);
 
-  useEffect(()=> {
-    async function getHotCollections(){
-      const fetchedData = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 0
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      }
+    ]
+  };
+
+  useEffect(() => {
+    async function getHotCollections() {
+      const fetchedData = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+      );
       setHotCollectionsData(fetchedData.data);
     }
     getHotCollections();
-
-  },[])
+  }, []);
 
   console.log(hotCollectionsData);
   return (
@@ -50,29 +88,42 @@ const HotCollections = () => {
               </div>
             </div>
           ))} */}
-          {hotCollectionsData.map(data=>(
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={data.id}>
-            <div className="nft_coll">
-              <div className="nft_wrap">
-                <Link to={`/item-details/${data.nftId}`}>
-                  <img src={data.nftImage} className="lazy img-fluid" alt="" />
-                </Link>
+          <Slider {...settings}>
+            {hotCollectionsData.map((data) => (
+              <div
+                // className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                key={data.id}
+              >
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to={`/item-details/${data.nftId}`}>
+                      <img
+                        src={data.nftImage}
+                        className="lazy img-fluid"
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to={`/author/${data.authorId}`}>
+                      <img
+                        className="lazy pp-coll"
+                        src={data.authorImage}
+                        alt=""
+                      />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{data.title}</h4>
+                    </Link>
+                    <span>ERC-{data.code}</span>
+                  </div>
+                </div>
               </div>
-              <div className="nft_coll_pp">
-                <Link to= {`/author/${data.authorId}`}>
-                  <img className="lazy pp-coll" src={data.authorImage} alt="" />
-                </Link>
-                <i className="fa fa-check"></i>
-              </div>
-              <div className="nft_coll_info">
-                <Link to="/explore">
-                  <h4>{data.title}</h4>
-                </Link>
-                <span>ERC-{data.code}</span>
-              </div>
-            </div>
-          </div>
-          ))}
+            ))}
+          </Slider>
         </div>
       </div>
     </section>
