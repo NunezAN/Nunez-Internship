06bf0353514df899a6f180e40051ceb997d0e782
NunezAN <alex101nunez@gmail.com>
2022-09-28 16:07:45 -0500

    data is now displaying skeleton state while fetching data

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -6,16 +6,17 @@ import axios from "axios";
 const TopSellers = () => {
   const [topSellers, setTopSellers] = useState([]); //state to store top sellers data
   const [isLoading, setIsLoading] = useState(false); //state to store is loading state
-  useEffect((()=>{
-    async function fetchTopSellers(){
+  useEffect(() => {
+    async function fetchTopSellers() {
       setIsLoading(true);
-      const fetchedData = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers");
+      const fetchedData = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+      );
       setTopSellers(fetchedData.data);
       setIsLoading(false);
     }
     fetchTopSellers();
-    console.log(topSellers);
-}),[])
+  }, []);
 
   return (
     <section id="section-popular" className="pb-5">
@@ -29,40 +30,41 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {isLoading ? new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              )) : topSellers.map((seller) => (
-                <li key={seller.id}>
-                  <div className="author_list_pp">
-                    <Link to={`/author/${seller.authorId}`}>
-                      <img
-                        className="lazy pp-author"
-                        src={seller.authorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to={`/author/${seller.authorId}`}>{seller.authorName}</Link>
-                    <span>{seller.price} ETH</span>
-                  </div>
-                </li>))}
+              {isLoading
+                ? new Array(12).fill(0).map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <div className="skeleton-box skeletonProfile"></div>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="author_list_info">
+                        <div>
+                          <div className="skeleton-box skeletonName"></div>
+                        </div>
+                        <div className="skeleton-box skeletonSubName"></div>
+                      </div>
+                    </li>
+                  ))
+                : topSellers.map((seller) => (
+                    <li key={seller.id}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${seller.authorId}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={seller.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${seller.authorId}`}>
+                          {seller.authorName}
+                        </Link>
+                        <span>{seller.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
         </div>
