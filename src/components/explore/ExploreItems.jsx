97bb70ac257f3fb13ba19e9fa1642e41578373fd
NunezAN <alex101nunez@gmail.com>
2022-09-29 15:25:58 -0500

    data is now being filtered using select form

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -10,23 +10,45 @@ const ExploreItems = () => {
   const [exploreItems, setExploreItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [showCards, setShowCards] = useState(8);
+  const [apiCall, setApiCall] = useState(
+    "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+  );
   useEffect(() => {
     async function fetchExploreItems() {
       setIsLoading(true);
-      const fetchedData = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
-      );
+      const fetchedData = await axios.get(apiCall);
       setExploreItems(fetchedData.data);
       setIsLoading(false);
     }
     fetchExploreItems();
-  }, []);
+  }, [apiCall]);
+  function filter(value) {
+    if(value === "DEFAULT") {
+      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore");
+    }
+    if(value === "price_low_to_high") {
+      console.log(value);
+      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_low_to_high");
+    }
+    if(value === "price_high_to_low") {
+      console.log(value);
+      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_high_to_low");
+    }
+    if(value === "likes_high_to_low") {
+      console.log(value);
+      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=likes_high_to_low");
+    }
+  }
 
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
-          <option value="">Default</option>
+        <select
+          id="filter-items"
+          defaultValue="DEFAULT"
+          onChange={(event) => filter(event.target.value)}
+        >
+          <option value="DEFAULT">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
@@ -70,7 +92,7 @@ const ExploreItems = () => {
             .map((item) => (
               <ExploreItemCard key={item.id} item={item}></ExploreItemCard>
             ))}
-      {showCards < exploreItems.length &&
+      {showCards < exploreItems.length && (
         <div className="col-md-12 text-center">
           <Link
             to=""
@@ -81,7 +103,7 @@ const ExploreItems = () => {
             Load more
           </Link>
         </div>
-      }
+      )}
     </>
   );
 };
