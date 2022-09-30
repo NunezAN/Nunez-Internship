70afe31fa115ef7882724f6370f15c8f3838b247
NunezAN <alex101nunez@gmail.com>
2022-09-30 12:04:47 -0500

    integrated skeleton loading state for author page and added follow/unfollow btn

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -5,6 +5,7 @@ import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 import { useEffect } from "react";
 import ExploreItemCard from "../UI/ExploreItemCard.jsx";
+import SkeletonExploreItemCard from "../UI/SkeletonExploreItemCard";
 
 const ExploreItems = () => {
   const [exploreItems, setExploreItems] = useState([]);
@@ -23,20 +24,28 @@ const ExploreItems = () => {
     fetchExploreItems();
   }, [apiCall]);
   function filter(value) {
-    if(value === "DEFAULT") {
-      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore");
+    if (value === "DEFAULT") {
+      setApiCall(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+      );
     }
-    if(value === "price_low_to_high") {
+    if (value === "price_low_to_high") {
       console.log(value);
-      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_low_to_high");
+      setApiCall(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_low_to_high"
+      );
     }
-    if(value === "price_high_to_low") {
+    if (value === "price_high_to_low") {
       console.log(value);
-      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_high_to_low");
+      setApiCall(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=price_high_to_low"
+      );
     }
-    if(value === "likes_high_to_low") {
+    if (value === "likes_high_to_low") {
       console.log(value);
-      setApiCall("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=likes_high_to_low");
+      setApiCall(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=likes_high_to_low"
+      );
     }
   }
 
@@ -54,44 +63,15 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {isLoading
-        ? new Array(8).fill(0).map((_, index) => (
-            <div
-              key={index}
-              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              style={{ display: "block", backgroundSize: "cover" }}
-            >
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <div className="skeleton-box skeletonProfile"></div>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                    </div>
-                  </div>
-                  <div className="skeleton-box skeletonNftImage"></div>
-                </div>
-                <div className="nft__item_info">
-                  <div>
-                    <div className="skeleton-box skeletonName"></div>
-                  </div>
-
-                  <div className="skeleton-box skeletonSubName"></div>
-                  <div className="nft__item_like">
-                    <div className="skeleton-box skeletonLike"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      {isLoading ? (
+        <SkeletonExploreItemCard />
+      ) : (
+        exploreItems
+          .slice(0, showCards)
+          .map((item) => (
+            <ExploreItemCard key={item.id} item={item}></ExploreItemCard>
           ))
-        : exploreItems
-            .slice(0, showCards)
-            .map((item) => (
-              <ExploreItemCard key={item.id} item={item}></ExploreItemCard>
-            ))}
+      )}
       {showCards < exploreItems.length && (
         <div className="col-md-12 text-center">
           <Link
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
 
 const Author = () => {
   const { id } = useParams(); //link param to get id
   const [authorData, setAuthorData] = useState([]); //state to store fetched author data
   const [isLoading, setIsLoading] = useState(true);
+  const [isFollowing, setIsFollowing] = useState(false);
 
   useEffect(() => {
     //function to fetch author data on mount
@@ -40,47 +40,91 @@ const Author = () => {
             <div className="row">
               <div className="col-md-12">
                 <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={authorData.authorImage} alt="" />
+                  {isLoading ? (
+                    <>
+                      <div className="de-flex-col">
+                        <div className="profile_avatar">
+                          <div className="skeleton-box skeletonProfile--author"></div>
+                          <i className="fa fa-check"></i>
+                          <div className="profile_name">
+                            <h4>
+                              <div>
+                                <div className="skeleton-box skeletonName"></div>
+                              </div>
+                              <div>
+                                <div className="skeleton-box skeletonSubName"></div>
+                              </div>
+                              <div>
+                                <div className="skeleton-box skeletonName--long"></div>
+                              </div>
+                            </h4>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="profile_follow de-flex">
+                        <div className="de-flex-col">
+                          <div className="skeleton-box skeleton--button"></div>
+                        </div>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="de-flex-col">
+                        <div className="profile_avatar">
+                          <img src={authorData.authorImage} alt="" />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          {authorData.authorName}
-                          <span className="profile_username">
-                            @{authorData.tag}
-                          </span>
-                          <span id="wallet" className="profile_wallet">
-                            {authorData.address}
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                          <i className="fa fa-check"></i>
+                          <div className="profile_name">
+                            <h4>
+                              {authorData.authorName}
+                              <span className="profile_username">
+                                @{authorData.tag}
+                              </span>
+                              <span id="wallet" className="profile_wallet">
+                                {authorData.address}
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>
+                            </h4>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">
-                        {authorData.followers} followers
+                      <div className="profile_follow de-flex">
+                        <div className="de-flex-col">
+                          <div className="profile_follower">
+                            {isFollowing
+                              ? authorData.followers + 1
+                              : authorData.followers}{" "}
+                            followers
+                          </div>
+                          {isFollowing ? (
+                            <Link
+                              to="#"
+                              className="btn-main"
+                              onClick={() => setIsFollowing(false)}
+                            >
+                              Unfollow
+                            </Link>
+                          ) : (
+                            <Link
+                              to="#"
+                              className="btn-main"
+                              onClick={() => setIsFollowing(true)}
+                            >
+                              Follow
+                            </Link>
+                          )}
+                        </div>
                       </div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
-                  </div>
+                    </>
+                  )}
                 </div>
               </div>
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  {isLoading ? (
-                    "Loading..."
-                  ) : (
-                    <AuthorItems author={authorData} />
-                  )}
+                  <AuthorItems isLoading={isLoading} author={authorData} />
                 </div>
               </div>
             </div>
